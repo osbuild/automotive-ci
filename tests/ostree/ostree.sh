cc0bcb136dd4e33ea64d36ea06033e0d3e1cfcee
Rachel Sibley <rasibley@redhat.com>
2021-06-09 14:52:59 -0400

    Add wrappers scripts to invoke rhel/edge CI tests for Automotive
    Add wrapper scripts to be invoked inside a Vagrant box, this will
    be used initially for briningup while we work towards onboarding and
    adapting the existing rhel/edge CI tests for Automotive CI

---
+++ tests/ostree/ostree.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+USER=$1
+PASS=$2
+
+# Setup subscription-manager to enable yum repos
+subscription-manager register --username $USER --password $PASS
+subscription-manager role --set="Red Hat Enterprise Linux Server"
+subscription-manager service-level --set="Self-Support"
+subscription-manager usage --set="Development/Test"
+subscription-manager attach
+
+# Install test dependencies
+dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm; sudo dnf config-manager --set-enabled epel
+dnf install -y ansible jq qemu-img qemu-kvm libvirt-client libvirt-daemon-kvm virt-install git
+git clone https://github.com/virt-s1/rhel-edge.git; cd rhel-edge
+chmod 600 key/ostree_key
+./ostree.sh
+
+echo "Done"
