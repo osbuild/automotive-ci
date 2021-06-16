72bd3fa3d58d888b7dfd7484af339d59bdc7d164
Miroslav Vadkerti <mvadkert@redhat.com>
2021-06-16 12:00:30 +0200

    Move to root
    Signed-off-by: Miroslav Vadkerti <mvadkert@redhat.com>

--- tests/ostree/cleanup.sh
+++ tests/ostree/cleanup.sh
@@ -2,5 +2,4 @@ sudo chmod 644 key/ostree_key
 virsh list --all | egrep -o 'osbuild[^ ]*' > domains
 cat domains | xargs -n1 virsh destroy
 cat domains | xargs -n1 virsh undefine --nvram
-rm -rf /var/tmp/tmt
 true
