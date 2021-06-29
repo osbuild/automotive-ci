3f8bd610b57c54a348388abf981b73699dd5030a
Juanje Ojeda <juanje@redhat.com>
2021-06-29 23:07:09 +0100

    test-vm: add some helpers for running test through ssh

--- tests/ci/test-vm.sh
+++ tests/ci/test-vm.sh
@@ -8,30 +8,51 @@ GUEST_ADDRESS=${GUEST_ADDRESS:-}
 SSH_KEY=${SSH_KEY:-}
 
 # SSH setup.
-SSH_OPTIONS=(-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ConnectTimeout=5)
+SSH_OPTIONS=(-q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ConnectTimeout=5 -i ${SSH_KEY} admin@${GUEST_ADDRESS})
+
+ssh_run () {
+    cmd="$*"
+    ssh "${SSH_OPTIONS[@]}" "/bin/bash -c '${cmd}'"
+}
 
 # Wait for the ssh server up to be.
 wait_for_ssh_up () {
-    SSH_STATUS=$(ssh "${SSH_OPTIONS[@]}" -i "${SSH_KEY}" admin@"${1}" '/bin/bash -c "echo -n READY"')
-    if [[ $SSH_STATUS == READY ]]; then
+    SSH_STATUS=$(ssh_run "echo -n READY")
+    if [[ "$SSH_STATUS" == READY ]]; then
         echo 1
     else
         echo 0
     fi
 }
 
-# Test result checking
-check_result () {
-    greenprint "Checking for test result"
-    if [[ $RESULTS == 1 ]]; then
-	greenprint "Cheking OS version"
-	ssh "${SSH_OPTIONS[@]}" -i "${SSH_KEY}" admin@"${GUEST_ADDRESS}" '/bin/bash -c "cat /etc/redhat-release"'
+# Helper function for the tests
+assert () {
+    local actual="$1"
+    local expected="$2"
+
+    if [[ "$actual" == "$expected" ]]; then
         greenprint "💚 Success"
     else
         greenprint "❌ Failed"
+        ssh_run "free -h ; df -h ; ps aux | grep edge ; journalctl -p err -n 100"
         exit 1
     fi
 }
+
+assert_process_running () {
+    process="$1"
+    processes=$(ssh_run "ps -u edge a | grep -v grep | grep -c ${process}") || true
+
+    assert "$processes" "1"
+}
+
+assert_package_installed () {
+    package="$1"
+    installed=$(ssh_run "rpm --quiet -q ${package} ; echo \$?")
+
+    assert "$installed" "0"
+}
+
 
 # Start VM.
 greenprint "Start VM"
@@ -41,8 +62,7 @@ virsh start "${IMAGE_KEY}"
 greenprint "🛃 Checking for SSH is ready to go"
 for LOOP_COUNTER in $(seq 0 30); do
     RESULTS="$(wait_for_ssh_up $GUEST_ADDRESS)"
-    if [[ $RESULTS == 1 ]]; then
-        echo "SSH is ready now! 🥳"
+    if [[ "$RESULTS" == 1 ]]; then
         break
     fi
     sleep 10
