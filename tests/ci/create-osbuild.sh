3c84f7476701406e8cb095e6542f8fa908213cbf
Robert O'Brien <roobrien@redhat.com>
2021-08-11 11:11:18 -0400

    move script to tests/ci; add manifests from gitlab

---
+++ tests/ci/create-osbuild.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -euo pipefail
+
+# following steps as noted on https://hackmd.io/ZbUOkeIXTjaP7XVpTSsrNw?view
+
+# Get OS data.
+source /etc/os-release
+
+ID=${ID:-}
+ARCH=$(arch)
+OS_VARIANT=${OS_VARIANT:-}
+IMAGE_TYPE=${IMAGE_TYPE:-}
+UUID=${UUID:-local}
+BOOT_LOCATION="http://mirror.centos.org/centos/8-stream/BaseOS/${ARCH}/os/"
+
+
+
+dnf -y copr enable @osbuild/osbuild
+SEARCH_PATTERN='baseurl=https://download.copr.fedorainfracloud.org/results/@osbuild/osbuild/epel-8-\$basearch/'
+REPLACE_PATTERN='baseurl=https://download.copr.fedorainfracloud.org/results/@osbuild/osbuild/centos-stream-8-$basearch/'
+sed -i -e "s|$SEARCH_PATTERN|$REPLACE_PATTERN|" \
+	/etc/yum.repos.d/_copr\:copr.fedorainfracloud.org\:group_osbuild\:osbuild.repo
+dnf -y install osbuild osbuild-tools
+
+
+dnf -y copr enable pingou/qtappmanager-fedora
+SEARCH_PATTERN='baseurl=https://download.copr.fedorainfracloud.org/results/pingou/qtappmanager-fedora/epel-8-$basearch/'
+REPLACE_PATTERN='baseurl=https://download.copr.fedorainfracloud.org/results/pingou/qtappmanager-fedora/centos-stream-8-$basearch/' 
+sed -i -e "s|$SEARCH_PATTERN|$REPLACE_PATTERN|" \
+	/etc/yum.repos.d/_copr:copr.fedorainfracloud.org:pingou:qtappmanager-fedora.repo
+
+
+osbuild-mpp files/cs8-qemu-${ARCH}.mpp.json cs8-qemu-${ARCH}.mpp.json.built
+
