d613d142257d3f1afe765c49755c5a6dd40d7a1d
Robert OBrien <roobrien@redhat.com>
2021-09-02 17:44:38 +0100

    new user 'edge' with no password new user 'admin' in wheel group Adding in files that composer adds in kickstart file:   /home/edge/.config/gdm-custom.conf   /home/edge/.config/autostart/neptune-ui.desktop   /etc/gdm/custom.conf

--- tests/ci/create-osbuild.sh
+++ tests/ci/create-osbuild.sh
@@ -11,6 +11,11 @@ ARCH=$(arch)
 UUID=${UUID:-local}
 DISK_IMAGE=${DISK_IMAGE:-"image_output/image/disk.img"}
 IMAGE_FILE=${IMAGE_FILE:-"/var/tmp/osbuild-${ARCH}-${UUID}.img"}
+PREPROCESSOR_FILE=osbuild-manifests/cs8/cs8-rpi4-tianocore-neptune.mpp.json
+OSBUILT_FILE=cs8-${ARCH}.mpp.json.built
+
+# TODO remove dnf clean when CI is running - this needed during development on re-used build machine
+#dnf clean all
 
 # install osbuild and osbuild-tools, which contains osbuild-mpp utility
 dnf -y copr enable @osbuild/osbuild
@@ -29,12 +34,23 @@ sed -i -e "s|$SEARCH_PATTERN|$REPLACE_PATTERN|" \
 	/etc/yum.repos.d/_copr:copr.fedorainfracloud.org:pingou:qtappmanager-fedora.repo
 
 
-
+echo "Calculating sha and base64 encoding files"
 # in order to add xorg conf file, we need to inject it into manifest, base64 encoded
-SHA=$(sha256sum files/xorg.conf | awk '{ print $1 }')
-BASE64=$(base64 -w0 files/xorg.conf)
-PREPROCESSOR_FILE=osbuild-manifests/cs8/cs8-rpi4-tianocore-neptune.mpp.json 
-OSBUILT_FILE=cs8-${ARCH}.mpp.json.built
+XORGSHA=$(sha256sum files/xorg.conf | awk '{ print $1 }')
+XORGBASE64=$(base64 -w0 files/xorg.conf)
+
+# add /etc/gdm/custom.conf
+GDMCONFSHA=$(sha256sum files/gdm-custom.conf | awk '{ print $1 }')
+GDMCONFBASE64=$(base64 -w0 files/gdm-custom.conf)
+
+# add /home/edge/.config/autostart/neptune3-ui.desktop
+NEPTUNE_SHA=$(sha256sum files/neptune3-ui.desktop | awk '{ print $1 }')
+NEPTUNE_BASE64=$(base64 -w0 files/neptune3-ui.desktop)
+
+# add /home/edge/.config/autostart/gnome-initial-setup-done
+echo 'yes' >  files/gnome-initial-setup-done
+GNOME_INITIAL_SETUP_SHA=$(sha256sum files/gnome-initial-setup-done | awk '{ print $1 }')
+GNOME_INITIAL_SETUP_BASE64=$(base64 -w0 files/gnome-initial-setup-done)
 
 # Generate a temporary SSH key
 SSH_KEY=files/tempkey
@@ -46,8 +62,14 @@ SSH_PUBLIC_KEY_CONTENT="$(< $SSH_PUBLIC_KEY)"
 echo "Preprocessing $PREPROCESSOR_FILE"
 # note - using ! as field separator as SSH_PUBLIC_KEY is a file path
 osbuild-mpp $PREPROCESSOR_FILE - \
-	| sed "s#XORGSHA#$SHA#" \
-	| sed "s#XORGBASE64#$BASE64#" \
+	| sed "s#XORGSHA#$XORGSHA#" \
+	| sed "s#XORGBASE64#$XORGBASE64#" \
+	| sed "s#GDMCONFSHA#$GDMCONFSHA#" \
+	| sed "s#GDMCONFBASE64#$GDMCONFBASE64#" \
+	| sed "s#NEPTUNE_SHA#$NEPTUNE_SHA#" \
+	| sed "s#NEPTUNE_BASE64#$NEPTUNE_BASE64#" \
+	| sed "s#GNOME_INITIAL_SETUP_SHA#$GNOME_INITIAL_SETUP_SHA#" \
+	| sed "s#GNOME_INITIAL_SETUP_BASE64#$GNOME_INITIAL_SETUP_BASE64#" \
 	| sed "s#TEMPSSHKEY#$SSH_PUBLIC_KEY_CONTENT#" \
 	> $OSBUILT_FILE
 
